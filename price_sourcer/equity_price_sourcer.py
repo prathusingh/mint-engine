3b52ad1557052a26c1cb7836ad74d94363407254
Prathu Singh <prathu@Prathus-MacBook-Pro.local>
2024-01-28 22:56:23 -0500

    feat: introducing sourcing equity from alphavantage

---
+++ price_sourcer/equity_price_sourcer.py
@@ -0,0 +1,19 @@
+import requests
+import pandas as pd
+
+from config import API_KEY
+
+symbol = 'AAPL'
+
+url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}'
+
+
+response = requests.get(url)
+data = response.json()
+
+if 'Time Series (Daily)' in data:
+    stock_data = pd.DataFrame(data['Time Series (Daily)'])
+
+    print(stock_data)
+else:
+    print("Error")
